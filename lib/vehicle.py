52ea19d35c99d23419b6e18c626d2b55ee08952f
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-07-17 13:17:32 +0300

    inheritance

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -4,7 +4,7 @@
 
 class Vehicle:
 
-    def __init__(self, wheel_size, wheel_number):
+    def __init__(self, wheel_size=22, wheel_number=18):
         self.wheel_size = wheel_size
         self.wheel_number = wheel_number
 
@@ -13,3 +13,13 @@ class Vehicle:
 
     def fill_up_tank(self):
         return "filling up!"
+
+my_vehicle = Vehicle()
+
+
+print(my_vehicle.wheel_size)       # Output: 22
+print(my_vehicle.wheel_number)     # Output: 18
+
+# Calling methods
+print(my_vehicle.go())             # Output: vrrrrrrrooom!
+print(my_vehicle.fill_up_tank()) 
